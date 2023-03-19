fcab7c016548d0620925e15c4cd531935ff47413
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-03-19 19:24:09 +0000

    some mistakes are corrected

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 "use strict";
-function kgToLbs(weight) {
-    if (typeof weight === "number")
-        return weight * 2.2;
-    else
-        return parseInt(weight) * 2.2;
+var _a;
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
 }
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let log = null;
+log === null || log === void 0 ? void 0 : log("a");
 //# sourceMappingURL=index.js.map
